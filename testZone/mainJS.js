0006d368bf0daddc7c716afddeb66e2c4f7fde11
Bohdan H <web.expert.plus@gmail.com>
2020-03-04 00:52:51 +0200

    updt

---
+++ testZone/mainJS.js
@@ -0,0 +1,9 @@
+//do NOT touch dis MFka
+'use strict'
+
+function myFunction() {
+  var element = document.getElementById("myDIV");
+  element.classList.add("text-typing2");
+  element.display = 'inline-block';
+}
+setTimeout(myFunction, 2000);
