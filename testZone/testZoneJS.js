3b6f7ee1b825dec203091fcf6745e3fbb84c6c1c
Bohdan H <web.expert.plus@gmail.com>
2020-03-03 18:14:20 +0200

    updt

--- testZone/testZoneJS.js
+++ testZone/testZoneJS.js
@@ -8,4 +8,18 @@ function myFunction() {
 setTimeout(myFunction, 2000);
 
 //TEST CODE HERE
-console.log(0 || "" || 2 || undefined || true || falsе);
+// don't change this code
+
+let  money = prompt('Ваш бюджет на месяц?', 1500),
+		 time = prompt('Дата в формате YYYY-MM-DD','2021-02-05'),
+		 answer1 = prompt('Введите обязательную статью расходов в этом месяце', "Еда"),
+		 answer2 = prompt('Во сколько обойдется?', 150),
+		 appData = {
+				budget: money,
+				timeData: time,
+				expenses: {answer1, answer2},
+				optionalExpenses: '',
+				income: '',
+				savings: false
+			};
+alert('Бюджет на месяц: '+money/30);
